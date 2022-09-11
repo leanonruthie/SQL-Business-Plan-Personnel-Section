47e38640fa77d59d096da8ea67b238718a53662f
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-09-11 18:17:35 -0400

    going back to the basics and building respective tables without any joining yet

--- db/schema.sql
+++ db/schema.sql
@@ -3,38 +3,21 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE department (
+CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department VARCHAR(30)
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager VARCHAR(30)
 );
 
 CREATE TABLE position (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   position VARCHAR(100),
-  salary DECIMAL (19,2),
-  department INT,
-  FOREIGN KEY (department)
-  REFERENCES department(id)
-  ON DELETE SET NULL
+  salary DECIMAL (19,2)
 );
 
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department VARCHAR(30)
+);
 
--- NOT WORKING
---SOURCE SCHEMA.SQL CONTINUALLY QUESTIONS THE FOREIGN KEYS. WHAT IS WRONG HERE?
--- CREATE TABLE employee (
---   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---   first_name VARCHAR(30),
---   last_name VARCHAR(30),
---   manager VARCHAR(30),
---   position INT,
---   FOREIGN KEY (position)
---   REFERENCES position(id),
---   department INT,
---   FOREIGN KEY (department)
---   REFERENCES department(id)
---   ON DELETE SET NULL
---   salary INT,
---   FOREIGN KEY (position)
---   REFERENCES position(id)
---   ON DELETE SET NULL
--- );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,25 @@
+INSERT INTO employee (first_name, last_name, manager)
+VALUES ("Keanu","Reeves", "Manager1"),
+    ("Stephanie","Courtney", "Manager2"),
+    ("Elon","Musk", "Manager3"),
+    ("Dave","Chappelle", "Manager4"),
+    ("Bethenny","Frankel", "Manager5"),
+    ("Jermaine","Cole", "Manager6"),
+    ("Oprah","Winfrey", "Manager7"),
+    ("Kim","Ruth", "Manager8"),
+    ("Doja","Cat", "Manager9");
+
+INSERT INTO position (position, salary)
+VALUES ("Backend Senior Engineer", 150000.00),
+    ("Service Representative", 50000.00),
+    ("Data Scientist", 95000.00),
+    ("DevOps Senior Engineer", 100000.00),
+    ("Hiring Manager", 70000.00),
+    ("Senior Associate", 90000.00),
+    ("Marketing Manager", 85000.00),
+    ("Sales Representative", 75000.00),
+    ("UX Designer", 80000.00);
+
 INSERT INTO department (department)
 VALUES ("Backend Engineering"),
     ("Customer Service"),
@@ -8,27 +30,3 @@ VALUES ("Backend Engineering"),
     ("Marketing"),
     ("Sales"),
     ("User Experience");
-
--- NOT WORKING
--- INSERT INTO position (first_name, last_name, position, salary, department, manager)
--- VALUES ("Keanu","Reeves", 1, 1, 1, "Manager1"),
---     ("Stephanie","Courtney", 2, 2, 2, "Manager2"),
---     ("Elon","Musk", 3, 3, 3, "Manager3"),
---     ("Dave","Chappelle", 4, 4, 4, "Manager4"),
---     ("Bethenny","Frankel", 5, 5, 5, "Manager5"),
---     ("Jermaine","Cole", 6, 6, 6, "Manager6"),
---     ("Oprah","Winfrey", 7, 7, 7, "Manager7"),
---     ("Kim","Ruth", 8, 8, 8, "Manager8"),
---     ("Doja","Cat", 9, 9, 9, "Manager9");
-
-INSERT INTO position (position, salary, department)
-VALUES ("Backend Senior Engineer", 150000.00, 1),
-    ("Service Representative", 50000.00, 2),
-    ("Data Scientist", 95000.00, 3),
-    ("DevOps Senior Engineer", 100000.00, 4),
-    ("Hiring Manager", 70000.00, 5),
-    ("Senior Associate", 90000.00, 6),
-    ("Marketing Manager", 85000.00, 7),
-    ("Sales Representative", 75000.00, 8),
-    ("UX Designer", 80000.00, 9);
-       
