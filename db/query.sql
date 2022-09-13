9ff83b8548e006f6d9033cf378c085459c940eb1
Ruth Kim <ruthkim@Ruths-MacBook-Air.local>
2022-09-13 09:44:51 -0400

    query.sql not necessary for final project but will leave here to prove successful testing of schema+seeds

--- db/query.sql
+++ db/query.sql
@@ -1,17 +1,12 @@
-SELECT positions.id AS id,
-positions.position_name AS position_name,
-positions.salary AS salary
-departments.department_name AS department_name
-FROM positions
-JOIN departments ON positions.department_id = departments.id;
-
 SELECT employees.id AS id,
 employees.first_name AS first_name,
 employees.last_name AS last_name,
-employees.manager AS manager,
 positions.position_name AS position_name,
 positions.salary AS salary,
-positions.department_id AS department_id
-FROM employees
-JOIN positions ON employees.position_id = positions.id;
-
+departments.department_name AS department_name,
+employees.manager AS manager
+  FROM departments
+  INNER JOIN positions
+  ON positions.department_id = departments.id
+  INNER JOIN employees
+  ON employees.position_id = positions.id;
